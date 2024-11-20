eeb1799a76a4351ee6fe45c3595b897556c6615a
Sarfeher <132908720+Sarfeher@users.noreply.github.com>
2024-11-20 11:06:01 +0100

    add tickets table and add key column to boards table

--- diligent-academy-assessments/final/backend/src/infrastructure/sqlite/init.ts
+++ diligent-academy-assessments/final/backend/src/infrastructure/sqlite/init.ts
@@ -11,15 +11,22 @@ async function setup() {
     DROP TABLE IF EXISTS boards;
   `);
 
+  //drop the tickets table if exist
+  await database.exec(`
+    DROP TABLE IF EXISTS tickets;
+    `)
+
   await database.exec(`
     DROP INDEX IF EXISTS idx_statuses_board_id;
   `);
 
+//add the key column to board
   await database.exec(`
     CREATE TABLE IF NOT EXISTS boards (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL UNIQUE,
       description TEXT,
+      key TEXT UNIQUE NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       deleted_at TIMESTAMP
@@ -40,16 +47,33 @@ async function setup() {
     );
   `);
 
+  //create tickets table 
+  await database.exec(`
+    CREATE TABLE IF NOT EXISTS tickets (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      description TEXT,
+      board_id INTEGER NOT NULL,
+      status_id INTEGER,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      deleted_at TIMESTAMP,
+      FOREIGN KEY (board_id) REFERENCES boards(id),
+      FOREIGN KEY (status_id) REFERENCES statuses(id)
+    );
+    `)
+
   await database.exec(`
     CREATE INDEX IF NOT EXISTS idx_statuses_board_id ON statuses(board_id);
   `);
 
+  //add key's and key values
   await database.exec(`
-    INSERT INTO boards (name, description) VALUES
-    ('Alpha Project', 'Description for Project Alpha'),
-    ('Beta Project', 'Description for Project Beta'),
-    ('Gamma Project', 'Description for Project Gamma'),
-    ('Delta Project', 'Description for Project Delta');
+    INSERT INTO boards (name, description, key) VALUES
+    ('Alpha Project', 'Description for Project Alpha', 'ALP'),
+    ('Beta Project', 'Description for Project Beta', 'BET'),
+    ('Gamma Project', 'Description for Project Gamma', 'GAM'),
+    ('Delta Project', 'Description for Project Delta', 'DEL');
   `);
 
   await database.exec(`
